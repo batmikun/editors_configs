bfb66b9a22b62c9b39c5286d78a2ce3285574369
Nicolas Stirnemann <nicolas@brainhi.com>
2022-09-01 13:17:28 -0300

    Feline WinBar

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,46 +1,46 @@
 local function set_color_theme()
-    vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+	vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
-    local colors = require("catppuccin.palettes").get_palette()
-    colors.none = "NONE"
+	local colors = require("catppuccin.palettes").get_palette()
+	colors.none = "NONE"
 
-    require("catppuccin").setup({
-	    custom_highlights = {
-		    Comment = { fg = colors.overlay1 },
-		    LineNr = { fg = colors.overlay1 },
-		    CursorLine = { bg = colors.none },
-		    CursorLineNr = { fg = colors.lavender },
-		    DiagnosticVirtualTextError = { bg = colors.none },
-		    DiagnosticVirtualTextWarn = { bg = colors.none },
-		    DiagnosticVirtualTextInfo = { bg = colors.none },
-		    DiagnosticVirtualTextHint = { bg = colors.none },
-	    },
-        integrations = {
-            telescope = true,
-            native_lsp = {
-                enabled = true,
-                virtual_text = {
-                    errors = { "italic" },
-                    hints = { "italic" },
-                    warnings = { "italic" },
-                    information = { "italic" },
-                },
-                underlines = {
-				    errors = { "underline" },
-				    hints = { "underline" },
-				    warnings = { "underline" },
-				    information = { "underline" },
-			    },
-            },
-            lsp_trouble = true,
-            treesitter = true,
-        },
-    })
+	require("catppuccin").setup({
+		custom_highlights = {
+			Comment = { fg = colors.overlay1 },
+			LineNr = { fg = colors.overlay1 },
+			CursorLine = { bg = colors.none },
+			CursorLineNr = { fg = colors.lavender },
+			DiagnosticVirtualTextError = { bg = colors.none },
+			DiagnosticVirtualTextWarn = { bg = colors.none },
+			DiagnosticVirtualTextInfo = { bg = colors.none },
+			DiagnosticVirtualTextHint = { bg = colors.none },
+		},
+		integrations = {
+			cmp = true,
+			telescope = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+			},
+			lsp_trouble = true,
+			treesitter = true,
+		},
+	})
 
-    vim.cmd [[colorscheme catppuccin]]
+	vim.cmd([[colorscheme catppuccin]])
 end
 
-require("catppuccin").setup {
-}
+require("catppuccin").setup({})
 
 set_color_theme()
