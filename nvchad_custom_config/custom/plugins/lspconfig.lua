5fcbc818862e9d36cf1f85a631c17329f29afdd5
Nicolas Stirnemann <nicolas@brainhi.com>
2022-09-12 17:46:21 -0300

    Return ols support

--- nvchad_custom_config/custom/plugins/lspconfig.lua
+++ nvchad_custom_config/custom/plugins/lspconfig.lua
@@ -4,6 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local servers = {
   "nimls",
+  "ols",
   "clangd",
   "gopls",
   "zls",
@@ -20,6 +21,24 @@ local servers = {
 }
 
 for _, lsp in ipairs(servers) do
+  if lsp == "nimls" then
+    lspconfig[lsp].setup {
+      cmd = { "nimlsp" },
+      filetypes = { "nim" },
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end
+
+  if lsp == "ols" then
+    lspconfig[lsp].setup {
+      cmd = { "ols" },
+      filetypes = { "odin" },
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end
+
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
