94ec50858b3c524e68c45926468ea2e7bbb29dab
Nicolas Stirnemann <nstirnemann59@gmail.com>
2022-09-11 22:47:47 -0300

    Added nvchad custom config

---
+++ nvchad_custom_config/custom/mappings.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "command mode", opts = { nowait = true } },
+    ["<leader>q"] = { ":q<CR>", "exit neovim"},
+    ["<space>bn"] = {":bn<CR>", "go to next buffer"}
+  },
+
+  i = {
+    ["jk"] = { "<ESC>", "escape vim" },
+  },
+}
+
+M.truzen = {
+  n = {
+    ["<leader>ta"] = { "<cmd> TZAtaraxis <CR>", "truzen ataraxis" },
+    ["<leader>tm"] = { "<cmd> TZMinimalist <CR>", "truzen minimal" },
+    ["<leader>tf"] = { "<cmd> TZFocus <CR>", "truzen focus" },
+  },
+}
+
+M.treesitter = {
+  n = {
+    ["<leader>cu"] = { "<cmd> TSCaptureUnderCursor <CR>", "find media" },
+  },
+}
+
+
+M.navigation = {
+  n = {
+    ["H"] = { "^", "beginning of line" },
+    ["L"] = { "$", "end of line" },
+  }
+}
+
+return M
