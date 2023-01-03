d715d7809274ac605eb062e850c4d8069e971733
Nicolas Stirnemann <nstirnemann59@gmail.com>
2023-01-02 23:51:02 -0300

    Added better UI

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -65,12 +65,49 @@ return packer.startup(function(use)
     -- Indent line
     use 'lukas-reineke/indent-blankline.nvim'
 
+    -- Better UI
+    -- Packer
+    use({
+        "folke/noice.nvim",
+        config = function()
+            require("noice").setup({
+                cmdline = {
+                    format = {
+                        cmdline = { icon = ">" },
+                        search_down = { icon = "🔍⌄" },
+                        search_up = { icon = "🔍⌃" },
+                        filter = { icon = "$" },
+                        lua = { icon = "☾" },
+                        help = { icon = "?" },
+                    },
+                },
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
+                },
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = true, -- add a border to hover docs and signature help
+                },
+            })
+        end,
+        requires = {
+            "MunifTanjim/nui.nvim",
+        }
+    })
+
     -- Themes - UI
     use "EdenEast/nightfox.nvim"
     use 'nanozuki/tabby.nvim'
     use {
         'feline-nvim/feline.nvim',
-        requires = {{ 'nvim-tree/nvim-web-devicons' }}
+        requires = { { 'nvim-tree/nvim-web-devicons' } }
     }
 
     -- Fuzzy Finder
